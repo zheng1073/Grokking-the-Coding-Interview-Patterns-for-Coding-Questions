999d903077af3e4067a65b3ab32dc4b31c0e2199
Jason Zheng <jason1996zheng@gmail.com>
2020-07-27 19:42:37 -0500

    Update Patterns_FastAndSlowPointers.py

--- Patterns_FastAndSlowPointers.py
+++ Patterns_FastAndSlowPointers.py
@@ -113,7 +113,45 @@ def find_start(head, cycle_length):
 Any number will be called a happy number if, after repeatedly replacing it with a number equal to the sum of the square of all of its digits, 
 leads us to number ‘1’. All other (not-happy) numbers will never reach ‘1’. Instead, they will be stuck in a cycle of numbers which does not include ‘1’.
 
+Time Complexity: O(logN)
+Space Complexity: O(1)
+"""
+def find_happy_number(num):
+  slow, fast = num, num
+  while True:
+    slow = find_square_sum(slow)  # move one step
+    fast = find_square_sum(find_square_sum(fast))  # move two steps
+    if slow == fast:  # found the cycle
+      break
+  return slow == 1  # see if the cycle is stuck on the number '1'
+
+
+def find_square_sum(num):
+  _sum = 0
+  while (num > 0):
+    digit = num % 10
+    _sum += digit * digit
+    num //= 10
+  return _sum 
+
+#Middle of the LinkedList (easy)
+"""
+Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+
+If the total number of nodes in the LinkedList is even, return the second middle node.
+
 Time Complexity:
 Space Complexity:
 """
-    
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+
+def find_middle_of_linked_list(head):
+  slow, fast = head, head
+  while (fast is not None and fast.next is not None):
+    slow = slow.next
+    fast = fast.next.next
+  return slow
