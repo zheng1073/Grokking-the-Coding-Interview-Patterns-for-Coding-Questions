4a3f5d24ee17f3c87a889913b48c580e68df8dc3
Jason Zheng <jason1996zheng@gmail.com>
2020-07-30 17:19:06 -0500

    Update Pattern_MergeIntervals.py

--- Pattern_MergeIntervals.py
+++ Pattern_MergeIntervals.py
@@ -14,7 +14,62 @@ Six ways 2 intervals can relate to each other.
 """
 Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
 
-Time Complexity: O(logN)
-Space Complexity: O(1)
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
 """
 from __future__ import print_function 
+
+class Interval:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+  
+  def print_interval(self):
+    print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+    
+def merge(intervals):
+  #edge case if there is only one interval
+  if len(intervals) < 2:
+    return intervals
+  
+  #sort interval based on the start
+  intervals.sort(key=lambda x: x.start)
+  
+  mergedIntervals = []
+  start = intervals[0].start
+  end = intervals[0].end
+  for i in range(1, len(intervals)):
+    interval = intervals[i]
+    #check if second interval is in our first interval
+    if interval.start <= end:
+      end = max(interval.end, end)
+    else:
+      mergedIntervals.append(Interval(start, end))
+      start = interval.start
+      end = interval.end
+  mergedIntervals.append(Interval(start, end))
+  return mergedIntervals
+
+#Insert Interval (medium)
+"""
+Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary 
+gintervals to produce a list that has only mutually exclusive intervals.
+
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
+"""
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
