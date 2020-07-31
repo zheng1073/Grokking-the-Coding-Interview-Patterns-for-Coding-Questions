d4017b63b53487a009d0b60958c3ef34ab51c2b3
Jason Zheng <jason1996zheng@gmail.com>
2020-07-31 15:45:08 -0500

    Create InsertInterval.py

---
+++ Pattern: Merge Intervals/InsertInterval.py
@@ -0,0 +1,7 @@
+#Insert Interval (medium)
+"""
+Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary 
+gintervals to produce a list that has only mutually exclusive intervals.
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
+"""
