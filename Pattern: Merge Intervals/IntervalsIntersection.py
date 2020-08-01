8281b44736792820743f2b9ff62df050bfeb7498
Jason Zheng <jason1996zheng@gmail.com>
2020-08-01 10:33:43 -0500

    Create IntervalsIntersection.py

---
+++ Pattern: Merge Intervals/IntervalsIntersection.py
@@ -0,0 +1,7 @@
+#Intervals Intersection (medium)
+"""
+Given two lists of intervals, find the intersection of these two lists. Each list consists of disjoint intervals sorted on their start time.
+
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
+"""
