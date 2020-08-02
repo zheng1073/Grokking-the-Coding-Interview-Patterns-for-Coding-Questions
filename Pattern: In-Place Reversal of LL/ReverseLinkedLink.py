b9f3826f88ff0ffd8ae196160d29a34494479e1c
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 14:51:15 -0500

    Create ReverseLinkedLink.py

---
+++ Pattern: In-Place Reversal of LL/ReverseLinkedLink.py
@@ -0,0 +1,8 @@
+
+#Reverse a LinkedList (easy)
+"""
+Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+"""
