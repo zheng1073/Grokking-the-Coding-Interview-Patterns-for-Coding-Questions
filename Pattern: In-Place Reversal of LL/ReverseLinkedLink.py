40c7c6bcb158d47e46ef7553c1c8cc11731d58e6
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 18:59:32 -0500

    Update ReverseLinkedLink.py

--- Pattern: In-Place Reversal of LL/ReverseLinkedLink.py
+++ Pattern: In-Place Reversal of LL/ReverseLinkedLink.py
@@ -6,3 +6,25 @@ Given the head of a Singly LinkedList, reverse the LinkedList. Write a function 
 Time Complexity: O(N)
 Space Complexity: O(1)
 """
+from __future__ import print_function
+
+class Node:
+  def __init__(self, value, next = None):
+    self.value = value
+    self.next = next
+   
+  def print_list(self):
+    temp = self
+    while temp is not None:
+      print(temp.value, end=" ")
+      temp = temp.next
+     print()
+    
+  def reverse(head):
+    previous, current, next = None, head, None
+    while current is not None:
+      next = current.next
+      current.next = previous
+      previous = current
+      current = next
+    return head
