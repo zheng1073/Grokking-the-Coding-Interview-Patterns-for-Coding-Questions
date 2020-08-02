848c0358fb2ac07164d4a689fdf24f7f2ecb9c66
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 12:24:01 -0500

    Update FindAllMissingNumber.py

--- Pattern: Cyclic Sort/FindAllMissingNumber.py
+++ Pattern: Cyclic Sort/FindAllMissingNumber.py
@@ -6,3 +6,30 @@ Find all those missing numbers.
 Time Complexity: O(N)
 Space Complexity: O(1)
 """
+def find_missing_numbers(nums):
+  i = 0
+  while i < len(nums):
+    j = nums[i] - 1
+    if nums[i] != nums[j]:
+      nums[i], nums[j] = nums[j], nums[i]  # swap
+    else:
+      i += 1
+
+  missingNumbers = []
+
+  for i in range(len(nums)):
+    if nums[i] != i + 1:
+      missingNumbers.append(i + 1)
+
+  return missingNumbers
+
+
+def find_missing_numbers(nums):
+  missingNumbers = []
+  # TODO: Write your code here
+  i = 0
+  while i < len(nums):
+    if i+1 not in nums:
+      missingNumbers.append(i+1)
+    i += 1
+  return missingNumbers
