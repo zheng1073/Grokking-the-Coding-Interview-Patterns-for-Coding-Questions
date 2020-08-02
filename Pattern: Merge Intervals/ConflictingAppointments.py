397b473d5fd71b74a2e3a5b9e662cad2ba1d0e2d
Jason Zheng <jason1996zheng@gmail.com>
2020-08-01 23:04:58 -0500

    Create ConflictingAppointments.py

---
+++ Pattern: Merge Intervals/ConflictingAppointments.py
@@ -0,0 +1,8 @@
+#Conflicting Appointments (medium)
+"""
+Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+
+Time Complexity: O(N + M)
+Space Complexity: O(1)
+"""
+def can_attend_all_appointments(intervals):
