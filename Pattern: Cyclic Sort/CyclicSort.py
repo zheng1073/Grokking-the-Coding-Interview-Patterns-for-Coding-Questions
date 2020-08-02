f2c7ae1a00fe4e1b7788422df27ec4a169c5ee52
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 01:34:02 -0500

    Update CyclicSort.py

--- Pattern: Cyclic Sort/CyclicSort.py
+++ Pattern: Cyclic Sort/CyclicSort.py
@@ -5,6 +5,17 @@ This means that the object with sequence number ‘3’ was created just before 
 Write a function to sort the objects in-place on their creation sequence number in O(n)O(n) and without any extra space. 
 For simplicity, let’s assume we are passed an integer array containing only the sequence numbers, though each number is actually an object.
 
-Time Complexity: O(Nlog(N))
-Space Complexity: O(N)
+Time Complexity: O(N)
+Space Complexity: O(1)
 """
+def cyclic_sort(nums):
+  # TODO: Write your code here
+  i = 1
+  while i < len(nums):
+    if nums[i] != i + 1:
+      rando = nums[i]
+      nums[i] = nums[rando-1]
+      nums[rando - 1] = rando
+    else:
+      i += 1
+  return nums
