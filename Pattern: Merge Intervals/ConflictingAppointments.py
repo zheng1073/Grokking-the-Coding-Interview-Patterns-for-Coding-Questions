debb9808d38f2be9d75ab28f196ab1cf496da70f
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 01:17:52 -0500

    Update ConflictingAppointments.py

--- Pattern: Merge Intervals/ConflictingAppointments.py
+++ Pattern: Merge Intervals/ConflictingAppointments.py
@@ -2,7 +2,15 @@
 """
 Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
 
-Time Complexity: O(N + M)
-Space Complexity: O(1)
+Time Complexity: O(Nlog(N))
+Space Complexity: O(N)
 """
 def can_attend_all_appointments(intervals):
+  intervals.sort(key=lambda x: x[0])
+  i = 0
+  rando = True
+  while rando and i<(len(intervals)-1):
+    if intervals[i+1][0] <= intervals[i][1]:
+      rando = False
+    i += 1
+  return rando
