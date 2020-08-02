2176656c49b59e8e4860c9aa4978ec17a9316955
Jason Zheng <jason1996zheng@gmail.com>
2020-08-02 14:47:36 -0500

    Update FindDulicateNumber.py

--- Pattern: Cyclic Sort/FindDulicateNumber.py
+++ Pattern: Cyclic Sort/FindDulicateNumber.py
@@ -6,3 +6,31 @@ Find that duplicate number without using any extra space. You are, however, allo
 Time Complexity: O(N)
 Space Complexity: O(1)
 """
+def find_duplicate(nums):
+  i = 0
+  while i < len(nums):
+    if nums[i] != i + 1:
+      j = nums[i] - 1
+      if nums[i] != nums[j]:
+        nums[i], nums[j] = nums[j], nums[i]  # swap
+      else:  # we have found the duplicate
+        return nums[i]
+    else:
+      i += 1
+
+  return -1
+
+
+def find_duplicate(nums):
+  # TODO: Write your code here
+  i = 0
+  rando = []
+  rando2 = True
+  while i < len(nums) and rando2:
+    if nums[i] not in rando:
+      rando.append(nums[i])
+      i += 1
+    else:
+      rando2 = False
+      return nums[i]  
+  return -1
