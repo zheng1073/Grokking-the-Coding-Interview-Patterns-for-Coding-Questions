07c75ef3c399535e293fe0b1c4f0d3f9d8662e75
Jason Zheng <jason1996zheng@gmail.com>
2020-08-03 16:07:22 -0500

    Create BinaryTreeLevelOrderTraversal.py

---
+++ Pattern: Tree Breadth First Search/BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,8 @@
+#Binary Tree Level Order Traversal (easy)
+"""
+Given a binary tree, populate an array to represent its level-by-level traversal. 
+You should populate the values of all nodes of each level from left to right in separate sub-arrays.
+
+Time Complexity: O(N)
+Space Complexity: O(W)
+"""
