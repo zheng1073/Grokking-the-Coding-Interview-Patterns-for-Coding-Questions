16ba7368d12c5b7a0626c4221bbedde2fe8fb779
Jason Zheng <jason1996zheng@gmail.com>
2020-08-03 20:34:58 -0500

    Create ZigzagTraversal.py

---
+++ Pattern: Tree Breadth First Search/ZigzagTraversal.py
@@ -0,0 +1,8 @@
+#Zipzag Traversal (medium)
+"""
+Given a binary tree, populate an array to represent its zigzag level order traversal. You should populate the values of all nodes of the first level from left to right, 
+then right to left for the next level and keep alternating in the same manner for the following levels.
+
+Time Complexity: O(N)
+Space Complexity: O(W)
+"""
