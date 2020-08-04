c4980caab55c06e89a4b0a2798a4b4cfe2e169ce
Jason Zheng <jason1996zheng@gmail.com>
2020-08-03 20:26:12 -0500

    Update BinaryTreeLevelOrderTraversal.py

--- Pattern: Tree Breadth First Search/BinaryTreeLevelOrderTraversal.py
+++ Pattern: Tree Breadth First Search/BinaryTreeLevelOrderTraversal.py
@@ -6,3 +6,29 @@ You should populate the values of all nodes of each level from left to right in 
 Time Complexity: O(N)
 Space Complexity: O(W)
 """
+from collections import deque
+
+class TreeNode:
+  def __init__(self, val):
+    self.val - val
+    self.left, self.right = None, None
+    
+def traverse(root):
+  result = []
+  if root is None:
+    return result
+  
+  queue = deque()
+  queue.append(root)
+  while queue:
+    currentLevel = []
+    for _ in range(len(queue)):
+      currentNode = queue.popleft()
+      currentLevel.append(currentNode.val)
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+    result.append(currentLevel)
+    
+  return result
