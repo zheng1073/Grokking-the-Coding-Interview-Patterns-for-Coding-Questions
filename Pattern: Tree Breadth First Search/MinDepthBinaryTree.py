ae7f826b98cc0697767fee131e7236b6b80604e2
Jason Zheng <jason1996zheng@gmail.com>
2020-08-04 17:00:14 -0500

    Create MinDepthBinaryTree.py

---
+++ Pattern: Tree Breadth First Search/MinDepthBinaryTree.py
@@ -0,0 +1,16 @@
+#Minimum Depth of a Binary Tree (easy)
+"""
+Find the minimum depth of a binary tree. The minimum depth is the number of nodes along the shortest path from the root node to the nearest leaf node.
+
+Time Complexity: O(N)
+Space Complexity: O(W)
+"""
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_minimum_depth(root):
+  # TODO: Write your code here
+  return -1
