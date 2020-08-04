f1264b0cd6a14eefdd02f307db6ad0a3294bc78b
Jason Zheng <jason1996zheng@gmail.com>
2020-08-03 21:40:46 -0500

    Update ZigzagTraversal.py

--- Pattern: Tree Breadth First Search/ZigzagTraversal.py
+++ Pattern: Tree Breadth First Search/ZigzagTraversal.py
@@ -6,3 +6,41 @@ then right to left for the next level and keep alternating in the same manner fo
 Time Complexity: O(N)
 Space Complexity: O(W)
 """
+from collections import deque
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+def traverse(root):
+  result = []
+  if root is None:
+    return result
+  
+  queue = deque()
+  queue.append(root)
+  leftToRight = True
+  while queue:
+    levelSize = len(queue)
+    currentLevel = deque()
+    for _ in range(levelSize):
+      currentNode = queue.popleft()
+
+      # add the node to the current level based on the traverse direction
+      if leftToRight:
+        currentLevel.append(currentNode.val)
+      else:
+        currentLevel.appendleft(currentNode.val)
+
+      # insert the children of current node in the queue
+      if currentNode.left:
+        queue.append(currentNode.left)
+      if currentNode.right:
+        queue.append(currentNode.right)
+
+    result.append(list(currentLevel))
+    # reverse the traversal direction
+    leftToRight = not leftToRight
+
+  return result
