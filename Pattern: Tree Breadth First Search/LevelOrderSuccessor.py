42392483c0daaf5c0390ba39ba023d54b14147c2
Jason Zheng <jason1996zheng@gmail.com>
2020-08-06 20:51:08 -0500

    Update LevelOrderSuccessor.py

--- Pattern: Tree Breadth First Search/LevelOrderSuccessor.py
+++ Pattern: Tree Breadth First Search/LevelOrderSuccessor.py
@@ -7,3 +7,30 @@ right after the given node in the level order traversal.
 Time Complexity: O(N)
 Space Complexity: O(W)
 """
+from collections import deque
+
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+
+def find_successor(root, key):
+  if root is None:
+    return None
+
+  queue = deque()
+  queue.append(root)
+  while queue:
+    currentNode = queue.popleft()
+    # insert the children of current node in the queue
+    if currentNode.left:
+      queue.append(currentNode.left)
+    if currentNode.right:
+      queue.append(currentNode.right)
+
+    # break if we have found the key
+    if currentNode.val == key:
+      break
+
+  return queue[0] if queue else None
