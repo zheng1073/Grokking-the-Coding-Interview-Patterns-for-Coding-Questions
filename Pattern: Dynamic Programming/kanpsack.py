660e1a67d0a083d4cf8ebc555d1af110e2b2b4a1
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 12:16:04 -0500

    Create kanpsack.py

---
+++ Pattern: Dynamic Programming/kanpsack.py
@@ -0,0 +1,7 @@
+#0/1 Knapsack (medium)
+"""
+Given a binary tree and a node, find the level order successor of the given node in the tree. The level order successor is the node that appears 
+right after the given node in the level order traversal.
+Time Complexity: O(N)
+Space Complexity: O(W)
+"""
