9cac3694f1bf84283a10a587a7f6aa7973a69624
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 21:25:28 -0500

    Update BinaryTreePathSum.py

--- Pattern: Tree Depth First Search/BinaryTreePathSum.py
+++ Pattern: Tree Depth First Search/BinaryTreePathSum.py
@@ -2,29 +2,24 @@
 """
 Given a binary tree and a number ‘S’, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals ‘S’.
 
-Time Complexity:
-Space Complexity:
+Time Complexity: O(N)
+Space Complexity: O(N)
 """
-using namespace std;
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
 
-#include <iostream>
 
-class TreeNode {
- public:
-  int val = 0;
-  TreeNode *left;
-  TreeNode *right;
-
-  TreeNode(int x) {
-    val = x;
-    left = right = nullptr;
-  }
-};
-
-class TreePathSum {
- public:
-  static bool hasPath(TreeNode *root, int sum) {
-    // TODO: Write your code here
-    return false;
-  }
-};
+def has_path(root, sum):
+  # TODO: Write your code here
+  if root is None:
+   return False
+  
+  #if current node is a leaf and its value is equal to the sum, we've found a path
+  if root.val == sum and root.left is None and root.right is None:
+   return Trye
+  
+  return has_path(root.left, sum-root.val) or has_path(root.right, sum-root.val)
+  return False
