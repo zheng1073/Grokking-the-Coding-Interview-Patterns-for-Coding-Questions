05f84702b167052695523b3ffb5274f8ce43ec10
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 22:13:42 -0500

    Create PathWithGivenSequence.py

---
+++ Pattern: Tree Depth First Search/PathWithGivenSequence.py
@@ -0,0 +1,17 @@
+#Path with Given Sequence (medium)
+"""
+Given a binary tree and a number sequence, find if the sequence is present as a root-to-leaf path in the given tree.
+
+Time Complexity: O(N^2)
+Space Complexity: O(NlogN)
+"""
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_path(root, sequence):
+  # TODO: Write your code here
+  return False  
