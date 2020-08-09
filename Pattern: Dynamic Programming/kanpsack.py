f62e4754b54a19995c981a64b6a76f6854818fd1
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 20:55:45 -0500

    Update kanpsack.py

--- Pattern: Dynamic Programming/kanpsack.py
+++ Pattern: Dynamic Programming/kanpsack.py
@@ -1,7 +1,111 @@
 #0/1 Knapsack (medium)
 """
-Given a binary tree and a node, find the level order successor of the given node in the tree. The level order successor is the node that appears 
-right after the given node in the level order traversal.
-Time Complexity: O(N)
-Space Complexity: O(W)
+Given the weights and profits of N items
+Put these items in a kapsack with capacity C
+
+GOAL: max profit
 """
+
+"""
+Given two integer arrays to represent weights and profits of ‘N’ items, we need to find a subset of these items which will give us maximum profit such that their 
+cumulative weight is not more than a given number ‘C’. Each item can only be selected once, which means either we put an item in the knapsack or we skip it.
+"""
+"""
+Basic Solution --> Brute-force solution
+
+Time Complexity: O(2^N)
+Space Complexity: O(N)
+"""
+def knapsack_recursive(profits, weights, capacity, currentIdex = 0):
+  #base checks
+  if capacity <= 0 or currentIdex >= len(profits):
+    return 0
+  
+  #recursive call after choosing the element at currentIndex
+  profit1 = 0
+  if weights[currentIndex] <= capacity:
+    profit1 = profits[currentIndex] + knapsack_recursive(profits, weights, capacity - weights[currentIndex], currentIdex + 1)
+    
+   profit2 = knapsack_recursive(profits, weights, capacity, currentIndex + 1)
+  
+  return max(profit1, profit2)
+
+"""
+memoization --> optimization technique used to speed up computing programs by storing the results of expensive function calls. 
+"""
+
+"""
+Top-down Dynamic Programming with Memoization
+  since we have 2 changing values, capacity and currentIndex, we can use a 2D array to sotre the results of all the solved sub-problems.
+
+Time Complexity: O(N*C)
+Space Complexity: O(N)
+"""
+def solve_knapsack(profits, weights, capacity):
+  # create a two dimensional array for Memoization, each element is initialized to '-1'
+  dp = [[-1 for x in range(capacity+1)] for y in range(len(profits))]
+  return knapsack_recursive(dp, profits, weights, capacity, 0)
+
+
+def knapsack_recursive(dp, profits, weights, capacity, currentIndex):
+
+  # base checks
+  if capacity <= 0 or currentIndex >= len(profits):
+    return 0
+
+  # if we have already solved a similar problem, return the result from memory
+  if dp[currentIndex][capacity] != -1:
+    return dp[currentIndex][capacity]
+
+  # recursive call after choosing the element at the currentIndex
+  # if the weight of the element at currentIndex exceeds the capacity, we
+  # shouldn't process this
+  profit1 = 0
+  if weights[currentIndex] <= capacity:
+    profit1 = profits[currentIndex] + knapsack_recursive(
+      dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1)
+
+  # recursive call after excluding the element at the currentIndex
+  profit2 = knapsack_recursive(
+    dp, profits, weights, capacity, currentIndex + 1)
+
+  dp[currentIndex][capacity] = max(profit1, profit2)
+  return dp[currentIndex][capacity]
+
+"""
+Bottom-up Dynamic Programming
+
+Time Complexity: O(N*C)
+Space Complexity: O(N)
+"""
+def solve_knapsack(profits, weights, capacity):
+  # basic checks
+  n = len(profits)
+  if capacity <= 0 or n == 0 or len(weights) != n:
+    return 0
+
+  dp = [[0 for x in range(capacity+1)] for y in range(n)]
+
+  # populate the capacity = 0 columns, with '0' capacity we have '0' profit
+  for i in range(0, n):
+    dp[i][0] = 0
+
+  # if we have only one weight, we will take it if it is not more than the capacity
+  for c in range(0, capacity+1):
+    if weights[0] <= c:
+      dp[0][c] = profits[0]
+
+  # process all sub-arrays for all the capacities
+  for i in range(1, n):
+    for c in range(1, capacity+1):
+      profit1, profit2 = 0, 0
+      # include the item, if it is not more than the capacity
+      if weights[i] <= c:
+        profit1 = profits[i] + dp[i - 1][c - weights[i]]
+      # exclude the item
+      profit2 = dp[i - 1][c]
+      # take maximum
+      dp[i][c] = max(profit1, profit2)
+
+  # maximum profit will be at the bottom-right corner.
+  return dp[n - 1][capacity]
