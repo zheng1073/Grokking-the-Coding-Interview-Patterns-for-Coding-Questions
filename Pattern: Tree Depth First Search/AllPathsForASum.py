16f4f300a4cac3c336a0001217f334ee2d381a9c
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 21:26:55 -0500

    Create AllPathsForASum.py

---
+++ Pattern: Tree Depth First Search/AllPathsForASum.py
@@ -0,0 +1,7 @@
+#All Paths for a Sum (medium)
+"""
+Given a binary tree and a number ‘S’, find all paths from root-to-leaf such that the sum of all the node values of each path equals ‘S’.
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+"""
