5952bdbf330eb6918c5a050d1dc4fea5bacd5c9c
Jason Zheng <jason1996zheng@gmail.com>
2020-08-09 00:37:38 -0500

    Update CountPathsForSum.py

--- Pattern: Tree Depth First Search/CountPathsForSum.py
+++ Pattern: Tree Depth First Search/CountPathsForSum.py
@@ -3,6 +3,41 @@
 Given a binary tree and a number ‘S’, find all paths in the tree such that the sum of all the node values of each path equals ‘S’. 
 Please note that the paths can start or end at any node but all paths must follow direction from parent to child (top to bottom).
 
-Time Complexity: O(N^2)
-Space Complexity: O(NlogN)
+Time Complexity: O(NlogN)
+Space Complexity: O(N)
 """
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def count_paths(root, S):
+  return count_paths_recursive(root, S, [])
+
+
+def count_paths_recursive(currentNode, S, currentPath):
+  if currentNode is None:
+    return 0
+
+  # add the current node to the path
+  currentPath.append(currentNode.val)
+  pathCount, pathSum = 0, 0
+  # find the sums of all sub-paths in the current path list
+  for i in range(len(currentPath)-1, -1, -1):
+    pathSum += currentPath[i]
+    # if the sum of any sub-path is equal to 'S' we increment our path count.
+    if pathSum == S:
+      pathCount += 1
+
+  # traverse the left sub-tree
+  pathCount += count_paths_recursive(currentNode.left, S, currentPath)
+  # traverse the right sub-tree
+  pathCount += count_paths_recursive(currentNode.right, S, currentPath)
+
+  # remove the current node from the path to backtrack
+  # we need to remove the current node while we are going up the recursive call stack
+  del currentPath[-1]
+
+  return pathCount
