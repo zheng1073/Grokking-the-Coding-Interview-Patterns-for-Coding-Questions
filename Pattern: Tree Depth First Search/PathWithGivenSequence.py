97beefe1579e9683a98bc8be27040208ddffb20b
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 22:23:10 -0500

    Update PathWithGivenSequence.py

--- Pattern: Tree Depth First Search/PathWithGivenSequence.py
+++ Pattern: Tree Depth First Search/PathWithGivenSequence.py
@@ -14,4 +14,28 @@ class TreeNode:
 
 def find_path(root, sequence):
   # TODO: Write your code here
-  return False  
+  if not root:
+    return len(sequence) == 0
+
+  return find_path_recursive(root, sequence, 0)
+
+
+def find_path_recursive(currentNode, sequence, sequenceIndex):
+
+  if currentNode is None:
+    return False
+
+  seqLen = len(sequence)
+  if sequenceIndex >= seqLen or currentNode.val != sequence[sequenceIndex]:
+    return False
+
+  # if the current node is a leaf, add it is the end of the sequence, we have found a path!
+  if currentNode.left is None and currentNode.right is None and sequenceIndex == seqLen - 1:
+    return True
+
+  # recursively call to traverse the left and right sub-tree
+  # return true if any of the two recursive call return true
+  return find_path_recursive(currentNode.left, sequence, sequenceIndex + 1) or \
+         find_path_recursive(currentNode.right, sequence, sequenceIndex + 1)
+  
+  
