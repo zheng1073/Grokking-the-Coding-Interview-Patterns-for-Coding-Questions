b1d72df0d5ba2920bd69e03de754a9a85eba8a35
Jason Zheng <jason1996zheng@gmail.com>
2020-08-08 21:50:37 -0500

    Create SumOfPathNumbers.py

---
+++ Pattern: Tree Depth First Search/SumOfPathNumbers.py
@@ -0,0 +1,19 @@
+
+#Sum of Path Numbers (medium)
+"""
+Given a binary tree where each node can only have a digit (0-9) value, each root-to-leaf path will represent a number. 
+Find the total sum of all the numbers represented by all paths.
+
+Time Complexity: O(N)
+Space Complexity: O(N)
+"""
+class TreeNode:
+  def __init__(self, val, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+
+def find_sum_of_path_numbers(root):
+  # TODO: Write your code here
+  return -1
